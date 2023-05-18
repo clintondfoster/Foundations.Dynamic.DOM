1068bddc99e19b09c7a7867635d8811b4a2a60ba
DanaPaolone <dpaolone13@gmail.com>
2023-05-18 18:21:04 -0400

    first successful render

--- index.js
+++ index.js
@@ -9,36 +9,48 @@ const list = document.querySelector("#list");
 const bookmarkList = [];
 const currentBookmark = {};
 
-const addBookmark = (bookmark) => {
-    const addName = nameInput.value;
-    const addLink = linkInput.value;
+const addBookmark = () => {
+  const addName = nameInput.value;
+  const addLink = linkInput.value;
+  const bookmark = {
+    bkmkName: addName,
+    link: addLink,
+  };
 
-    bookmarkList.push(bookmark)
-}
+  bookmarkList.push(bookmark);
+  console.log("SINGLE BOOKMARK: ", bookmark);
+
+  renderBookmarkList();
+
+  console.log("BOOMARK LIST: ", bookmarkList);
+};
+
 //add render function
 
+//render one bookmark object
+const renderSingleBookmark = (singleBookmark) => {
+  const bkMkLi = document.createElement("li");
+  bkMkLi.innerText = singleBookmark.link;
 
+  return bkMkLi;
+};
+
+//THEN render objects in the array into an UL
+const renderBookmarkList = () => {
+  const bkMkUl = document.createElement("ul");
+  const elements = bookmarkList.map((bookmark) =>
+    renderSingleBookmark(bookmark)
+  );
+  list.replaceChildren(...elements);
+  return elements;
+};
 
 //Eventlister to add & remove button, render after each click
 
-
 addButton.addEventListener("click", () => {
-    addBookmark(bookmark)});
-
-// const newObject = () => { 
-// }
-
-
+  addBookmark();
+});
 
 //Re-render entire list anytime bookmark is added or removed
-const renderList = () => {
-    const listArray = document.createElement('ul');
-    const listItem = document.createElement('li');
-    listArray.appendChild(listItem);
-
-    linkInput.textContent = linkInput.value
-}
-
 
 //Use map method to map the bookmark array to DOM elements
-
